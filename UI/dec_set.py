45c4d94c5edc2c4bc706bf4ecb37b59865b91ff9
DeftArtisan <81402620+DeftArtisan@users.noreply.github.com>
2023-05-21 09:03:49 +0300

    fix formatting

--- UI/dec_set.py
+++ UI/dec_set.py
@@ -1,5 +1,6 @@
 import os
 import math
+import sys
 
 _divisor = 12
 
@@ -20,11 +21,15 @@ _textual_sa:list = [
 ]
 
 def _exemp_s():
-    for _ in range(0, round(len(_textual_sa[0]) / (_divisor / 5))):
-        _textual_sa.append(_textual_sa[1])
-    with open('_grid_.txt', 'w') as _wr_fl:
-        _wr_fl.write(
-          ("".join([str((x + x) * (round(len(_textual_sa[0]) / (_divisor + 10)))).replace("-", "*") + "\n" for x in _textual_sa]))
-        )
+    try:
+        for _ in range(0, round(len(_textual_sa[0]) / (_divisor / 5))):
+           _textual_sa.append(_textual_sa[1])
 
-_exemp_s()
+        with open('_grid_.txt', 'w') as _wr_fl:
+           _wr_fl.write(
+           ("".join([str((x + x) * (round(len(_textual_sa[0]) / (_divisor + 10)))).replace("-", "*") + "\n" for x in _textual_sa]))
+           )
+    except:
+        sys.stderr.write("Inexistent or ill-formed/named file")
+
+ _exemp_s()
