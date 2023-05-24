631cdd51baea4698e4cd804de2ac4a16a0676269
DeftArtisan <81402620+DeftArtisan@users.noreply.github.com>
2023-05-24 18:13:50 +0300

    Update and rename utils_gd.h to utils_gr.h

---
+++ object_headers/utils_gr.h
@@ -0,0 +1,13 @@
+#ifndef "UTILS_GR"
+#define "UTILS_GR"
+#include "definitive.h"
+
+extern void _res_curs_pos(int _vr, int _vrr);
+
+extern void _hd_buff_crs();
+
+extern void buff_render(char16 _grid[12][100]);
+
+extern void _buffer_db(char16 _grid_d[12][100], char16 _grid[12][100]);
+
+#endif
